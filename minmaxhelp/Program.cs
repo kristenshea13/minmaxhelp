92ac7b068d8614b1a3f8f495b80c86e3dbd1ddc0
Kristen Shea <kstar61@live.com>
2019-03-30 14:33:03 -0500

    /

--- minmaxhelp/Program.cs
+++ minmaxhelp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace minmaxhelp
 {
@@ -6,7 +9,109 @@ namespace minmaxhelp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            Student student = CreateNewStudent();
+            List<Student> students = new List<Student>();
+            students.Add(student);
+
+
+            //List<Student> students = CreateNewStudent();
+            //Student students = new Student();
+            //students = CreateNewStudent(line);
+
+            foreach (var item in students)
+            {
+                Console.WriteLine($"{student.Name} Min: {student.GetMinimumScore()} Max: {student.GetMaximumScore()}");
+            }
+
+
+            Console.ReadLine();
+        
+
+
+
+
+
+    }
+
+        private static Student CreateNewStudent()
+        {
+            List<string> students = GetStudentDataFile();
+            //List<int> s = new List<int>();
+            //List<string> scores = new List<string>();
+
+            //int[] s = Array.Empty<int>();
+
+
+
+            // string[] scores = new string[properties.Length - 1];
+            string[] scores = Array.Empty<string>();
+
+            string name = "";
+
+            int[] s = Array.Empty<int>();
+            
+
+            foreach (string line in students)
+            {
+                string[] properties = line.Split(" ");
+                scores = new string[properties.Length - 1];
+
+
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    
+
+                    if (i == 0)
+                    {
+                        name = properties[0];
+                    }
+                    else
+                    {
+                        scores[i - 1] = properties[i];
+
+                    }
+
+                    //s = scores.ConvertAll(x => Int32.Parse(x));
+
+
+
+
+                    
+                }
+
+                
+
+            }
+
+            s = new int[scores.Length];
+
+            s = Array.ConvertAll(scores, int.Parse);
+            Student student = new Student(name, s);
+            //int[] s = new int[scores.Length];
+
+
+            return student;
+
+
+
+
+
+        }
+
+        private static List<string> GetStudentDataFile()
+        {
+            List<string> students = new List<string>();
+            foreach (string line in File.ReadAllLines(@"C:\Users\kstar\source\repos\minmaxhelp\minmaxhelp\studentdata.txt"))
+            {
+                students.Add(line);
+            }
+            return students;
+            //List<string> lines = File.ReadAllLines(@"c:studentdata.txt").ToList();
+            //return lines;
+
+        }
+
         }
     }
-}
---
+++ minmaxhelp/Student.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace minmaxhelp
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public int[] Scores { get; set; }
+
+        
+
+        public Student(string name, int[] scores)
+        {
+            Name = name;
+            Scores = scores;
+        }
+
+        public int GetMaximumScore()
+        {
+            int maxScore = 0;
+            for (int i = 0; i < Scores.Length; i++)
+            {
+                if (Scores[i] < maxScore)
+                {
+                    maxScore = Scores[i];
+                }
+            }
+            return maxScore;
+        }
+
+        public int GetMinimumScore()
+        {
+            int minScore = 100;
+            for (int i = 0; i < Scores.Length; i++)
+            {
+                if (minScore < Scores[i])
+                {
+                    minScore = Scores[i];
+                }
+            }
+            return minScore;
+        }
+    }
+}
